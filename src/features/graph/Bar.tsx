bf06996b7237f27dde8a9f24ba00f6a59bd03701
Mirko Kirović <mirko@etf.rs>
2019-07-15 22:05:48 +0200

    fix bad coordinates

--- src/features/graph/Bar.tsx
+++ src/features/graph/Bar.tsx
@@ -12,9 +12,9 @@ const Bar = ({ data, color }: BarProps) => {
       {data.slice(0, data.length - 1).map((a, index) => (
         <line
           x1={30 * index + 15}
-          y1={data[index] * 1.5 + 150}
+          y1={-data[index] * 1.5 + 150}
           x2={30 * (index + 1) + 15}
-          y2={data[index + 1] * 1.5 + 150}
+          y2={-data[index + 1] * 1.5 + 150}
           key={`${a}-${index}`}
           stroke={color}
         />
